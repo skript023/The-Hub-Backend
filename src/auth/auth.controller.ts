3ef17d8820eb65a322982f32803c55d27d694829
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-04-01 22:10:11 +0700

    add response html

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -66,6 +66,21 @@ export class AuthController {
             secure: cookie_param.secure,
             sameSite: cookie_param.sameSite,
             expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        }).send({ message: 'Login success' }); 
+        }).send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Success Message</title>
+        </head>
+        <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh;">
+        <div class="success-message" style="background-color: #dff0d8; border: 1px solid #d6e9c6; color: #3c763d; padding: 20px; border-radius: 5px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
+        <p>Success: Authentication Successful!</p>
+        </div>
+        </body>
+        </html>
+
+        `); 
     }
 }
