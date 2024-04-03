55fe17d6cc706882046482d80d461560beb2986b
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-04-03 19:17:32 +0700

    test build

--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -11,7 +11,7 @@ import { SheetsAppendResponse } from './dto/google/response-append.dto';
 import { ValueInputOption } from './enum/google/value_input_option';
 import { InsertDataOption } from './enum/google/insert_data_option';
 import { credentials } from 'src/util/config/service_account';
-// import { google, sheets_v4 } from 'googleapis';
+import { google, sheets_v4 } from 'googleapis';
 
 @Injectable()
 export class AttendanceService 
@@ -21,78 +21,55 @@ export class AttendanceService
 		private response: response<Attendance>
 	)
 	{
-		// const auth = new google.auth.GoogleAuth({
-		// 	credentials,
-		// 	scopes: ['https://www.googleapis.com/auth/spreadsheets'], // Adjust scopes as needed
-		// });
+		const auth = new google.auth.GoogleAuth({
+			credentials,
+			scopes: ['https://www.googleapis.com/auth/spreadsheets'], // Adjust scopes as needed
+		});
 
-		// this.sheets = google.sheets({ version: 'v4', auth });
+		this.sheets = google.sheets({ version: 'v4', auth });
 	}
 
-	private defaultRow = 3;
-	// private sheets: sheets_v4.Sheets;
+	private sheets: sheets_v4.Sheets;
 
 	async create(req: any, createAttendanceDto: CreateAttendanceDto) 
 	{
-		// const data = {
-		// 	"range": `${date.getCurrentMonth()}!A:E`,
-		// 	"majorDimension": "ROWS",
-		// 	"values": [
-		// 		[
-		// 			date.indonesiaFormat(new Date(createAttendanceDto.date)),
-		// 			createAttendanceDto.type,
-		// 			createAttendanceDto.jenis,
-		// 			createAttendanceDto.deskripsi,
-		// 			createAttendanceDto.durasi
-		// 		]
-		// 	]
-		// }
+		const data = {
+			"range": `${date.getCurrentMonth()}!A:E`,
+			"majorDimension": "ROWS",
+			"values": [
+				[
+					date.indonesiaFormat(new Date(createAttendanceDto.date)),
+					createAttendanceDto.type,
+					createAttendanceDto.jenis,
+					createAttendanceDto.deskripsi,
+					createAttendanceDto.durasi
+				]
+			]
+		}
 
-		// const response = await this.sheets.spreadsheets.values.append({
-		// 	spreadsheetId: '1H5YjdyNwvyYPZizfeS2A7l8dfiIKI_yffh2DxLNGpYc',
-		// 	range: `${date.getCurrentMonth()}!A:E`, // Specify the range
-		// 	valueInputOption: ValueInputOption.USER_ENTERED,
-		// 	includeValuesInResponse: true,
-		// 	insertDataOption: InsertDataOption.INSERT_ROWS,
-		// 	requestBody: data,
-		// });
+		const response = await this.sheets.spreadsheets.values.append({
+			spreadsheetId: '1H5YjdyNwvyYPZizfeS2A7l8dfiIKI_yffh2DxLNGpYc',
+			range: `${date.getCurrentMonth()}!A:E`, // Specify the range
+			valueInputOption: ValueInputOption.USER_ENTERED,
+			includeValuesInResponse: true,
+			insertDataOption: InsertDataOption.INSERT_ROWS,
+			requestBody: data,
+		});
 
-		// if (response.status != 200)
-		// {
-		// 	throw new HttpException(`${response.data}`, response.status);
-		// }
+		if (response.status != 200)
+		{
+			throw new HttpException(`${response.data}`, response.status);
+		}
 
-		// const sheetsResponse = response.data;
-		// createAttendanceDto.range = sheetsResponse.updates.updatedRange;
+		const sheetsResponse = response.data;
+		createAttendanceDto.range = sheetsResponse.updates.updatedRange;
 
-		// const attendance = await this.attendanceModel.create(createAttendanceDto);
+		const attendance = await this.attendanceModel.create(createAttendanceDto);
 
-		// this.response.success = true;
-		// this.response.message = `Success checked attendance at ${attendance.date}`;
+		this.response.success = true;
+		this.response.message = `Success checked attendance at ${attendance.date}`;
 
-		// return this.response.json();
-	}
-
-	async findAllInSheets(req: any) 
-	{
-		// const config: AxiosRequestConfig = {
-		// 	headers: {
-		// 		Authorization: `Bearer ${req.user.remember_token}`,
-		// 	},
-		// };
-
-		// const response = await axios.get(`${google.sheets}/1H5YjdyNwvyYPZizfeS2A7l8dfiIKI_yffh2DxLNGpYc/values/April!A4:E`, config);
-		
-		// if (response.status != 200)
-		// {
-		// 	throw new HttpException(`${response.data}`, response.status);
-		// }
-
-		// this.response.success = true;
-		// this.response.message = `Success get attendance`;
-		// this.response.data = response.data;
-
-		// return this.response.json();
+		return this.response.json();
 	}
 
 	async findAll()
@@ -129,72 +106,72 @@ export class AttendanceService
 
 	async update(id: string, updateAttendanceDto: UpdateAttendanceDto, req: any) 
 	{
-		// const attendance = await this.attendanceModel.findById(id);
+		const attendance = await this.attendanceModel.findById(id);
 
-		// const params = `?valueInputOption=${ValueInputOption.USER_ENTERED}&includeValuesInResponse=true&key=${process.env.GOOGLE_API_KEY}`;
+		const params = `?valueInputOption=${ValueInputOption.USER_ENTERED}&includeValuesInResponse=true&key=${process.env.GOOGLE_API_KEY}`;
 
-		// const config: AxiosRequestConfig = {
-		// 	headers: {
-		// 		'Content-Type': 'application/json'
-		// 	},
-		// };
+		const config: AxiosRequestConfig = {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+		};
 
-		// const data = {
-		// 	"range": `${attendance.range}`,
-		// 	"majorDimension": "ROWS",
-		// 	"values": [
-		// 		[
-		// 			date.indonesiaFormat(new Date(updateAttendanceDto.date)),
-		// 			updateAttendanceDto.type,
-		// 			updateAttendanceDto.jenis,
-		// 			updateAttendanceDto.deskripsi,
-		// 			updateAttendanceDto.durasi
-		// 		]
-		// 	]
-		// }
+		const data = {
+			"range": `${attendance.range}`,
+			"majorDimension": "ROWS",
+			"values": [
+				[
+					date.indonesiaFormat(new Date(updateAttendanceDto.date)),
+					updateAttendanceDto.type,
+					updateAttendanceDto.jenis,
+					updateAttendanceDto.deskripsi,
+					updateAttendanceDto.durasi
+				]
+			]
+		}
 		
-		// const response = await this.sheets.spreadsheets.values.update({
-		// 	spreadsheetId: '1H5YjdyNwvyYPZizfeS2A7l8dfiIKI_yffh2DxLNGpYc',
-		// 	range: `${attendance.range}`, // Specify the range
-		// 	valueInputOption: ValueInputOption.USER_ENTERED,
-		// 	includeValuesInResponse: true,
-		// 	requestBody: data,
-		// });
+		const response = await this.sheets.spreadsheets.values.update({
+			spreadsheetId: '1H5YjdyNwvyYPZizfeS2A7l8dfiIKI_yffh2DxLNGpYc',
+			range: `${attendance.range}`, // Specify the range
+			valueInputOption: ValueInputOption.USER_ENTERED,
+			includeValuesInResponse: true,
+			requestBody: data,
+		});
 
-		// if (response.status != 200)
-		// {
-		// 	throw new HttpException(`${response.data}`, response.status);
-		// }
+		if (response.status != 200)
+		{
+			throw new HttpException(`${response.data}`, response.status);
+		}
 
-		// const updatedAttendance = await this.attendanceModel.findByIdAndUpdate(id, updateAttendanceDto);
+		const updatedAttendance = await this.attendanceModel.findByIdAndUpdate(id, updateAttendanceDto);
 
-		// this.response.success = true;
-		// this.response.message = `Success checked attendance at ${updatedAttendance.date}`;
+		this.response.success = true;
+		this.response.message = `Success checked attendance at ${updatedAttendance.date}`;
 
-		// return this.response.json();
+		return this.response.json();
 	}
 
 	async remove(id: string, req: any) 
 	{
-		// const attendance = await this.attendanceModel.findById(id);
+		const attendance = await this.attendanceModel.findById(id);
 
-        // if (!attendance) throw new NotFoundException('Attendance not found, unable to delete');
+        if (!attendance) throw new NotFoundException('Attendance not found, unable to delete');
 
-		// const response = await this.sheets.spreadsheets.values.clear({
-		// 	spreadsheetId: '1H5YjdyNwvyYPZizfeS2A7l8dfiIKI_yffh2DxLNGpYc',
-		// 	range: `${attendance.range}`,
-		// });
+		const response = await this.sheets.spreadsheets.values.clear({
+			spreadsheetId: '1H5YjdyNwvyYPZizfeS2A7l8dfiIKI_yffh2DxLNGpYc',
+			range: `${attendance.range}`,
+		});
 
-		// if (response.status != 200)
-		// {
-		// 	throw new HttpException(`${response.data}`, response.status);
-		// }
+		if (response.status != 200)
+		{
+			throw new HttpException(`${response.data}`, response.status);
+		}
 
-		// const deletedAttendance = await this.attendanceModel.findByIdAndDelete(id);
+		const deletedAttendance = await this.attendanceModel.findByIdAndDelete(id);
 
-		// this.response.message = `Attendance at ${deletedAttendance.date} successfully deleted`;
-        // this.response.success = true;
+		this.response.message = `Attendance at ${deletedAttendance.date} successfully deleted`;
+        this.response.success = true;
 
-        // return this.response.json();
+        return this.response.json();
 	}
 }
