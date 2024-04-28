abccd53dcb64325d18cd45368223b5351e221df1
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-04-28 23:15:19 +0700

    change login with google

--- src/auth/google/google.strategy.ts
+++ src/auth/google/google.strategy.ts
@@ -18,8 +18,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 			prompt: 'consent',
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_SECRET,
-			callbackURL: 'http://localhost:3000/auth/google/redirect',
-			scope: ['email', 'profile', 'https://www.googleapis.com/auth/spreadsheets'],
+			callbackURL: 'https://server.rena.my.id/auth/google/redirect',
+			scope: ['email', 'profile'],
 		});
 	}
 
