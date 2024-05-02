464c8e5f7ff8cb8f24fb0711417b78c6b1e37b30
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-05-02 12:20:29 +0700

    change cors

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const options = {
+        cors: true,
+    }
+    const app = await NestFactory.create(AppModule, options);
     app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe());
-    app.enableCors({
-        origin: true,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-        credentials: true,
-    });
+    // app.enableCors({
+    //     origin: true,
+    //     methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
+    //     credentials: true,
+    // });
     await app.listen(3000);
 }
 bootstrap();
