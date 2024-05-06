e647ecc3412c3a6819fdbd46653f305d54b277a1
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-05-06 15:56:37 +0700

    fix indentasi

--- src/util/whitelist/cors.ts
+++ src/util/whitelist/cors.ts
@@ -2,27 +2,27 @@ import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.int
 import { Logger } from '@nestjs/common';
 
 export const whitelist = [
-    'https://www.rena.my.id',
-    'https://dashboard.rena.my.id',
-    'http://localhost:5173',
+	'https://www.rena.my.id',
+	'https://dashboard.rena.my.id',
+	'http://localhost:5173',
 ]
 
 export const options: CorsOptions = {
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1) 
-        {
-          Logger.log(`Allow request from ${origin}`, 'Whitelist');
+	credentials: true,
+	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+	allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+	origin: (origin, callback) => {
+		if (whitelist.indexOf(origin) !== -1) 
+		{
+			Logger.log(`Allow request from ${origin}`, 'Whitelist');
 
-          callback(null, true);
-        }
-        else 
-        {
-          Logger.warn(`Blocked request from ${origin}`);    
+			callback(null, true);
+		}
+		else 
+		{
+			Logger.warn(`Blocked request from ${origin}`);    
 
-          callback(new Error('Not allowed by CORS'));
-        }
-    },
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
 }
